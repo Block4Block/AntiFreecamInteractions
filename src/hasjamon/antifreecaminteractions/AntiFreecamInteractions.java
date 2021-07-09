52bebfdaacf4441a0dde2b7507aee93e25294ece
Bahm <x.benjamin.ahm@gmail.com>
2021-07-09 18:45:09 +0700

    Might prevent more edge cases now.

--- src/hasjamon/antifreecaminteractions/AntiFreecamInteractions.java
+++ src/hasjamon/antifreecaminteractions/AntiFreecamInteractions.java
@@ -23,27 +23,29 @@ public class AntiFreecamInteractions extends JavaPlugin implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void onInteract(PlayerInteractEvent e){
         if(e.getAction() == Action.RIGHT_CLICK_BLOCK)
-            if(notLookingAtBlock(e.getClickedBlock(), e.getPlayer()))
+            if(!lookingAtBlock(e.getClickedBlock(), e.getPlayer()))
                 e.setCancelled(true);
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onBlockBreak(BlockBreakEvent e){
-        if(notLookingAtBlock(e.getBlock(), e.getPlayer()))
+        Block b = e.getBlock();
+
+        if(b.getType() != Material.ANDESITE && !lookingAtBlock(b, e.getPlayer()))
             e.setCancelled(true);
     }
 
-    private boolean notLookingAtBlock(Block b, Player p){
+    private boolean lookingAtBlock(Block b, Player p){
         if(b != null){
             Location pEyeLoc = p.getEyeLocation();
             Vector direction = pEyeLoc.getDirection();
 
             RayTraceResult result = p.getWorld().rayTraceBlocks(pEyeLoc, direction, 6.0);
 
-            if(result != null && result.getHitBlock() != null)
-                return !result.getHitBlock().equals(b);
+            if(result == null || result.getHitBlock() == null || !result.getHitBlock().equals(b))
+                return false;
         }
 
-        return false;
+        return true;
     }
 }
